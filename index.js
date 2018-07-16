b8fd0f1b4476fb770f7e29a380bec95085b47fec
Sophie <sophie.hughes07@gmail.com>
2018-07-16 04:11:00 +0100

    adjusted js so that when one section opens, the other closes

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
-var btn = document.getElementById('about');
-var text = document.getElementById('aboutTxt');
-var height = text.clientHeight;
-var width = text.clientWidth;
-console.log(width + 'x' + height);
-text.style.height = height + 'px';
-text.style.width = width + 'px';
+var btnAbt = document.getElementById('about');
+var textAbt = document.getElementById('aboutTxt');
+var heightAbt = textAbt.clientHeight;
+var widthAbt = textAbt.clientWidth;
+console.log(widthAbt + 'x' + heightAbt);
+textAbt.style.height = heightAbt + 'px';
+textAbt.style.width = widthAbt + 'px';
 
-btn.addEventListener('click', function() {
-  if(text.style.visibility == 'hidden') {
-    text.style.visibility = 'visible';
-    text.style.height = height + 'px';
-    text.style.width = width + 'px';
-    hint.style.padding = '.5em';
+var btnPro = document.getElementById('projects');
+var textPro = document.getElementById('projectsTxt');
+var heightPro = textPro.clientHeight;
+var widthPro = textPro.clientWidth;
+console.log(widthPro + 'x' + heightPro);
+textPro.style.height = heightPro + 'px';
+textPro.style.width = widthPro + 'px';
+
+btnAbt.addEventListener('click', function() {
+  if(textAbt.style.visibility == 'hidden') {
+    if(textPro.style.visibility !== 'hidden') {
+      textPro.style.visibility = 'hidden';
+      textPro.style.height = '0';
+      textPro.style.width = '0';
+      textPro.style.padding = '0';
+    }
+    textAbt.style.visibility = 'visible';
+    textAbt.style.height = heightAbt + 'px';
+    textAbt.style.width = widthAbt + 'px';
+    textAbt.style.padding = '.5em';
   } else {
-    text.style.visibility = 'hidden';
-    text.style.height = '0';
-    text.style.width = '0';
-    text.style.padding = '0';
+    textAbt.style.visibility = 'hidden';
+    textAbt.style.height = '0';
+    textAbt.style.width = '0';
+    textAbt.style.padding = '0';
+  }
+});
+
+btnPro.addEventListener('click', function() {
+  if(textPro.style.visibility == 'hidden') {
+    if(textAbt.style.visibility !== 'hidden') {
+      textAbt.style.visibility = 'hidden';
+      textAbt.style.height = '0';
+      textAbt.style.width = '0';
+      textAbt.style.padding = '0';
+    }
+    textPro.style.visibility = 'visible';
+    textPro.style.height = heightPro + 'px';
+    textPro.style.width = widthPro + 'px';
+    textPro.style.padding = '.5em';
+  } else {
+    textPro.style.visibility = 'hidden';
+    textPro.style.height = '0';
+    textPro.style.width = '0';
+    textPro.style.padding = '0';
   }
 });
