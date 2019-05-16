cbfe245831505b2e07ade5b289ff3ca828399aea
Sophie <sophie.hughes07@gmail.com>
2019-05-16 21:02:03 +0100

    Added FitText to name

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-var btnAbt = document.getElementById('about');
-var textAbt = document.getElementById('aboutTxt');
-var heightAbt = textAbt.clientHeight;
-var widthAbt = textAbt.clientWidth;
-console.log(widthAbt + 'x' + heightAbt);
-textAbt.style.height = heightAbt + 'px';
-textAbt.style.width = widthAbt + 'px';
-
-var btnPro = document.getElementById('projects');
-var textPro = document.getElementById('projectsTxt');
-var heightPro = textPro.clientHeight;
-var widthPro = textPro.clientWidth;
-console.log(widthPro + 'x' + heightPro);
-textPro.style.height = heightPro + 'px';
-textPro.style.width = widthPro + 'px';
-textPro.style.visibility = 'hidden';
-textPro.style.height = '0';
-textPro.style.width = '0';
-textPro.style.padding = '0';
-textAbt.style.visibility = 'hidden';
-textAbt.style.height = '0';
-textAbt.style.width = '0';
-textAbt.style.padding = '0';
-
 var clickEvent = (function() {
   if ('ontouchstart' in document.documentElement === true)
     return 'touchstart';
@@ -29,45 +5,15 @@ var clickEvent = (function() {
     return 'click';
 })();
 
-btnAbt.addEventListener(clickEvent, function () {
-  if (textAbt.style.visibility == 'hidden') {
-    if (textPro.style.visibility !== 'hidden') {
-      textPro.style.visibility = 'hidden';
-      textPro.style.height = '0';
-      textPro.style.width = '0';
-      textPro.style.padding = '0';
-    }
-    textAbt.style.visibility = 'visible';
-    textAbt.style.height = heightAbt + 'px';
-    textAbt.style.width = widthAbt + 'px';
-    textAbt.style.padding = '.5em';
-  } else {
-    textAbt.style.visibility = 'hidden';
-    textAbt.style.height = '0';
-    textAbt.style.width = '0';
-    textAbt.style.padding = '0';
-  }
+$('aboutTxt').on('show.bs.collapse', function () {
+  $('projectsTxt').collapse('hide');
 });
 
-btnPro.addEventListener(clickEvent, function () {
-  if (textPro.style.visibility == 'hidden') {
-    if (textAbt.style.visibility !== 'hidden') {
-      textAbt.style.visibility = 'hidden';
-      textAbt.style.height = '0';
-      textAbt.style.width = '0';
-      textAbt.style.padding = '0';
-    }
-    textPro.style.visibility = 'visible';
-    textPro.style.height = heightPro + 'px';
-    textPro.style.width = widthPro + 'px';
-    textPro.style.padding = '.5em';
-  } else {
-    textPro.style.visibility = 'hidden';
-    textPro.style.height = '0';
-    textPro.style.width = '0';  }
+$('projectsTxt').on('show.bs.collapse', function () {
+  $('aboutTxt').collapse('hide');
 });
 
-
+$("#name").fitText(1.0, {minFontSize: '40px'});
 
 function mouseOut() {
   document.getElementById('name').innerHTML = 'Sophie Hughes';
